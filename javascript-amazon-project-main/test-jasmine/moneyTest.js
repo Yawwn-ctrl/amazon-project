fc9edb3679092884f3bcbdf61c7440b4beeb2e48
Yawwn-ctrl <sahilparte49@gmail.com>
2026-08-16 13:53:38 +0530

    added money tests

--- javascript-amazon-project-main/test-jasmine/moneyTest.js
+++ javascript-amazon-project-main/test-jasmine/moneyTest.js
@@ -1,2 +1,12 @@
 import {formatCurrency} from '../scripts/utils/money.js';
-describe('test
+describe('test  suite: formatCurrency', () => {
+  it('convert cents to dollars', () => {
+    expect(formatCurrency(2029)).toEqual('$20.29');
+  });
+  it('works with 0', () => {
+    expect(formatCurrency(0)).toEqual('$0.00');
+  });
+  it('rounds to nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('$20.01');
+  });
+});
--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -1,18 +1,21 @@
 import {formatCurrency} from '../scripts/utils/money.js';
   console.log('test suite: formatCurrency');
+  console.log('convert cents to dollars');
+
 if (formatCurrency(2029) === '$20.29') {
   console.log('formatCurrency test passed');
 } else {
   console.error('formatCurrency test failed');
-}
+}   
 
+console.log('works with 0');
 
 if (formatCurrency(0) === '$0.00') {
   console.log('formatCurrency test passed');
 } else {
   console.error('formatCurrency test failed');
 }
-
+console.log('rounds to nearest cent');
 if (formatCurrency(2000.5) === '$20.01') {
   console.log('formatCurrency test passed');
 } else {
